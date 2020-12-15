646f34e7d32b69d4d559b853bd5639dfe1d981fc
rainbowmoonlight <46021863+rainbowmoonlight@users.noreply.github.com>
2020-12-14 17:54:32 -0800

    update NATO project

--- Intermediate Day 26 NATO Alphabet/main.py
+++ Intermediate Day 26 NATO Alphabet/main.py
@@ -9,7 +9,14 @@ phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 print(phonetic_dict)
 
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+def generate_phonetic():
+	word = input("Enter a word: ").upper()
+	try:
+		output_list = [phonetic_dict[letter] for letter in word]
+	except KeyError:
+		print("Sorry, only letters in the alphabet please")
+		generate_phonetic()
+	else:
+		print(output_list)
 
-word = input("Enter a word: ").upper()
-output_list = [phonetic_dict[letter] for letter in word]
-print(output_list)
+generate_phonetic()
