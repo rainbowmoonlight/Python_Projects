5890e61299b6cbe9874b5503cf7083b2aa6ab200
rainbowmoonlight <46021863+rainbowmoonlight@users.noreply.github.com>
2020-12-21 17:36:20 -0800

    update kanye quote main

--- Day 33 ISS Overheard Notifier/kanye quotes/main.py
+++ Day 33 ISS Overheard Notifier/kanye quotes/main.py
@@ -1,9 +1,12 @@
 from tkinter import *
-
+import requests
 
 def get_quote():
-    pass
-    #Write your code here.
+    response = requests.get("https://api.kanye.rest")
+    response.raise_for_status()
+    data = response.json()
+    quote=data["quote"]
+    canvas.itemconfig(quote_text, text = quote)
 
 
 
